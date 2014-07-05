34d68833cfd15ef5e5996e934a2b333c1cbc1109
ChepKun <ProgrammerKun@gmail.com>
2014-07-05 03:32:29 +0300

    update

---
+++ config/common_full.mk
@@ -0,0 +1,5 @@
+# copy prebuilt
+PRODUCT_COPY_FILES += \
+    vendor/theta/prebuilt/lib/libdstox.so:system/lib/libdstox.so \
+    vendor/theta/prebuilt/bin/swap:system/bin/swap \
+    vendor/theta/prebuilt/bin/fstrim:system/bin/fstrim
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+export OTA_DEVELOPER_ID=thetaorb
+
 if [[ "$THETA_PATCH_SOURCE" -eq 1 ]]
 then
   echo ""
@@ -9,9 +11,3 @@ then
   echo ""
   export THETA_PATCH_SOURCE=0
 fi
-
-
-#for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-10.2 | awk {'print $1'})
-#do
-#    add_lunch_combo $combo
-#done
