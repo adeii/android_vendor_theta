b706e5c2d1b2d68c81ba698afd0f0a1d35b4a76b
ChepKun <ProgrammerKun@gmail.com>
2014-07-20 01:15:06 +0300

    updates

--- config/common_full.mk
+++ config/common_full.mk
@@ -2,4 +2,5 @@
 PRODUCT_COPY_FILES += \
     vendor/theta/prebuilt/lib/libdstox.so:system/lib/libdstox.so \
     vendor/theta/prebuilt/bin/swap:system/bin/swap \
-    vendor/theta/prebuilt/bin/fstrim:system/bin/fstrim
+    vendor/theta/prebuilt/bin/fstrim:system/bin/fstrim \
+    vendor/theta/prebuilt/etc/hosts:system/etc/hosts
