13245059426fe15abb7963ebf394faa03789c6dd
ChepKun <ProgrammerKun@gmail.com>
2014-07-23 18:18:54 +0300

    update patches, add default build.prop params

--- config/common_full.mk
+++ config/common_full.mk
@@ -4,3 +4,9 @@ PRODUCT_COPY_FILES += \
     vendor/theta/prebuilt/bin/swap:system/bin/swap \
     vendor/theta/prebuilt/bin/fstrim:system/bin/fstrim \
     vendor/theta/prebuilt/etc/hosts:system/etc/hosts
+
+# common properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.strictmode.disable=true \
+    ro.max.fling_velocity=12000 \
+    windowsmgr.max_events_per_sec=120
