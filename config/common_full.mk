593a16611feb8cde360f7f5eb46c5d0e7c090ab5
ChepKun <ProgrammerKun@gmail.com>
2014-07-31 00:06:03 +0300

    updates

--- config/common_full.mk
+++ config/common_full.mk
@@ -10,3 +10,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.strictmode.disable=true \
     ro.max.fling_velocity=12000 \
     windowsmgr.max_events_per_sec=120
+
+# init.d scripts
+PRODUCT_COPY_FILES += \
+    vendor/theta/prebuilt/etc/init.d/99fstrim:system/etc/init.d/99fstrim
